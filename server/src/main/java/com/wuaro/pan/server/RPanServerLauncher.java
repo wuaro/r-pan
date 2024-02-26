acbb3409c40473d78c7b13445dbba76c5238ee3b
wuhuarou <59736073+wuaro@users.noreply.github.com>
2024-02-26 17:45:24 +0800

    配置项目入参校验器validator

--- server/src/main/java/com/wuaro/pan/server/RPanServerLauncher.java
+++ server/src/main/java/com/wuaro/pan/server/RPanServerLauncher.java
@@ -8,12 +8,16 @@ import io.swagger.annotations.Api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotNull;
 
 @SpringBootApplication(scanBasePackages = {RPanConstants.BASE_COMPONENT_SCAN_PATH})
 @ServletComponentScan(basePackages = RPanConstants.BASE_COMPONENT_SCAN_PATH)
 @RestController
-@Api("测试接口")
+@Api("测试接口类")
+@Validated
 public class RPanServerLauncher {
 
     public static void main(String[] args) {
@@ -21,7 +25,7 @@ public class RPanServerLauncher {
     }
 
     @GetMapping("hello")
-    public R hello(@RequestParam(value = "name", required = false) String name) {
+    public R<String> hello(@NotNull(message = "name不能为空") String name) {
         return R.data("hello " + name + "!");
     }
 
