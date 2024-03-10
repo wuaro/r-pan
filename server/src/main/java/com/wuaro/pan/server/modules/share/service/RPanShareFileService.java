800327fa7b9fbed91b0c1b98d1d694fcdbcacc09
wuhuarou <59736073+wuaro@users.noreply.github.com>
2024-03-10 11:22:49 +0800

    搭建本地缓存模块

---
+++ server/src/main/java/com/wuaro/pan/server/modules/share/service/RPanShareFileService.java
@@ -0,0 +1,13 @@
+package com.wuaro.pan.server.modules.share.service;
+
+import com.wuaro.pan.server.modules.share.entity.RPanShareFile;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 11391
+* @description 针对表【r_pan_share_file(用户分享文件表)】的数据库操作Service
+* @createDate 2024-03-06 09:03:12
+*/
+public interface RPanShareFileService extends IService<RPanShareFile> {
+
+}
