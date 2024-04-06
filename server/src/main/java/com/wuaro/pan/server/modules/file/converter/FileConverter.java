d0ff4bb70b5203062b77373f4b59d1c42f83c110
wuaro <15543099417@163.com>
2024-04-06 21:16:27 +0800

    实现创建新文件夹，重命名文件夹两个功能

--- server/src/main/java/com/wuaro/pan/server/modules/file/converter/FileConverter.java
+++ server/src/main/java/com/wuaro/pan/server/modules/file/converter/FileConverter.java
@@ -14,14 +14,47 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface FileConverter {
 
+
+    /**
+     * CreateFolderPO转CreateFolderContext
+     *
+     * @param createFolderPO
+     * @return
+     */
+    /*
+    注解：
+        1. @Mapping(target = "parentId", expression = "java(com.wuaro.pan.core.utils.IdUtil.decrypt(createFolderPO.getParentId()))")
+            这表示将 CreateFolderPO 对象中的 parentId 属性映射到 CreateFolderContext 对象中的同名属性，
+            并使用 IdUtil.decrypt 方法对 parentId 进行解密操作。
+            说白了就是：将CreateFolderPO的parentId变量，解密后 映射给 CreateFolderContext中的parentId
+        2. @Mapping(target = "userId", expression = "java(com.wuaro.pan.server.common.utils.UserIdUtil.get())")
+            这表示将调用 UserIdUtil.get() 方法获得的用户ID 映射为 CreateFolderContext 对象中的 userId 属性。
+        这些映射关系可以在对象转换时方便地进行属性赋值，使得代码更加简洁和可读。
+     */
     @Mapping(target = "parentId", expression = "java(com.wuaro.pan.core.utils.IdUtil.decrypt(createFolderPO.getParentId()))")
     @Mapping(target = "userId", expression = "java(com.wuaro.pan.server.common.utils.UserIdUtil.get())")
     CreateFolderContext createFolderPO2CreateFolderContext(CreateFolderPO createFolderPO);
 
-//    @Mapping(target = "fileId", expression = "java(com.wuaro.pan.core.utils.IdUtil.decrypt(updateFilenamePO.getFileId()))")
-//    @Mapping(target = "userId", expression = "java(com.wuaro.pan.server.common.utils.UserIdUtil.get())")
-//    UpdateFilenameContext updateFilenamePO2UpdateFilenameContext(UpdateFilenamePO updateFilenamePO);
-//
+    /**
+     * UpdateFilenamePO转UpdateFilenameContext
+     *
+     * @param updateFilenamePO
+     * @return
+     */
+    /*
+    注解：
+        1. @Mapping(target = "fileId", expression = "java(com.wuaro.pan.core.utils.IdUtil.decrypt(updateFilenamePO.getFileId()))")
+            这表示将 updateFilenamePO 对象中的 fileId 属性映射到 UpdateFilenameContext 对象中的同名属性，
+            并使用 IdUtil.decrypt 方法对 userId 进行解密操作。
+            说白了就是：UpdateFilenamePO 的 userId 变量，解密后 映射给 UpdateFilenameContext 中的 userId
+        2. @Mapping(target = "userId", expression = "java(com.wuaro.pan.server.common.utils.UserIdUtil.get())")
+            这表示将调用 UserIdUtil.get() 方法获得的用户ID 映射为 UpdateFilenameContext 对象中的 userId 属性。
+        这些映射关系可以在对象转换时方便地进行属性赋值，使得代码更加简洁和可读。
+     */
+    @Mapping(target = "fileId", expression = "java(com.wuaro.pan.core.utils.IdUtil.decrypt(updateFilenamePO.getFileId()))")
+    @Mapping(target = "userId", expression = "java(com.wuaro.pan.server.common.utils.UserIdUtil.get())")
+    UpdateFilenameContext updateFilenamePO2UpdateFilenameContext(UpdateFilenamePO updateFilenamePO);
+
 //    @Mapping(target = "userId", expression = "java(com.wuaro.pan.server.common.utils.UserIdUtil.get())")
 //    DeleteFileContext deleteFilePO2DeleteFileContext(DeleteFilePO deleteFilePO);
 //
