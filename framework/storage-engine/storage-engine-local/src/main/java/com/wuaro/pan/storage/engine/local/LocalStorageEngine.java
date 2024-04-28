d49a33f879b26b8e78107754c3a501568dbf45a2
wuaro <15543099417@163.com>
2024-04-28 16:55:33 +0800

    搭建文件存储引擎的抽象 搭架子

---
+++ framework/storage-engine/storage-engine-local/src/main/java/com/wuaro/pan/storage/engine/local/LocalStorageEngine.java
@@ -0,0 +1,19 @@
+package com.wuaro.pan.storage.engine.local;
+
+import com.wuaro.pan.storage.engine.core.AbstractStorageEngine;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * 本地文件存储引擎实现类
+ */
+@Component
+public class LocalStorageEngine extends AbstractStorageEngine {
+
+
+}
